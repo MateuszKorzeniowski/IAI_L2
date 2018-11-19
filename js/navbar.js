1b2ae4b25deadb531b27b9de4eaf640d6da3d300
xradziox <31067@student.pwsztar.edu.pl>
2018-11-19 18:04:12 +0100

    add new folder js and css

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+let nav= '<ul><li><a href="index.html">Strona Główna</a></li>'
+    +'<li><a href="map.html">Mapa</a></li>'
+    +'<li><a href="contact.html">Kontakt</a></li></ul>';
+
+let divNav=document.querySelector(".navbar");
+
+divNav.innerHTML=nav;
--- navbar.js
+++
@@ -1,7 +0,0 @@
-let nav= '<nav><ul><li><a href="index.html">Strona Główna</a></li>'
-    +'<li><a href="map.html">Mapa</a></li>'
-    +'<li><a href="contact.html">Kontakt</a></li></ul></nav>';
-
-let divNav=document.querySelector(".navbar");
-
-divNav.innerHTML=nav;
