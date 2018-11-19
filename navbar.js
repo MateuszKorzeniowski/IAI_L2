0b4719897ced62634d906ed618dcb42131ae18f4
xradziox <31067@student.pwsztar.edu.pl>
2018-11-19 17:45:01 +0100

    add navbar.js

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav= '<nav><ul><li><a href="index.html">Strona Główna</a></li>'
+    +'<li><a href="map.html">Mapa</a></li>'
+    +'<li><a href="contact.html">Kontakt</a></li></ul></nav>';
+
+let divNav=document.querySelector(".navbar");
+
+divNav.innerHTML=nav;
